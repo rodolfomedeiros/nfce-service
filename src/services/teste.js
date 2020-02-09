f336221b4eb18638139a1d9559b2ae14f16b5b00
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-02-08 21:59:55 -0300

    ja pegando os items da nota

--- src/services/teste.js
+++ src/services/teste.js
@@ -1,18 +1,30 @@
 const puppeteer = require('puppeteer');
 const url = 'https://nfce.set.rn.gov.br/portalDFE/nota-potiguar/nfce-mobile-danfe.aspx?chNFCe=24200210670811001044651010000360479291169643&token=69FF4439A9FBE92CF215E7722A1B6187';
-const options = {
-    headless: true
-};
-const selector = 'span#tbItensList_lblTbItensCodigo_';
+
+const selector = 'table#tbItensList tr';
 (async function(){
-    const browser = await puppeteer.launch(options);
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     const numbers = await page.$$eval(selector, nodes => {
-        return nodes.map(node => {
-            return node.textContent;
-        })
+        return nodes.map(
+            node => {
+                let values = [];
+                let childs =  node.childNodes;
+                childs.forEach(
+                     child => {
+                        let value = child.textContent.trim();
+                        if(value != ''){
+                            values.push(value)
+                        }
+                     }
+                )
+                return values;
+            }
+        )
     });
+
+    
     console.log(numbers);
     await browser.close();
 })();
